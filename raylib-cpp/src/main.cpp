ba62c2503a1e28dbdd06cf829cb693cdab2e3fb0
Ferdian <ferdian.artwork@gmail.com>
2023-01-05 12:24:10 +0700

    done

--- raylib-cpp/src/main.cpp
+++ raylib-cpp/src/main.cpp
@@ -3,33 +3,51 @@
 #include <raylib.h>
 #include <rlImGui.h>
 
+#if defined(PLATFORM_WEB)
+    #include <emscripten/emscripten.h>
+#endif
+
+void mainLoop(void);
+
 int main()
 {
-	std::cout << "hello world\n";
 
-	InitWindow(512, 512, "Starter Raylib");
-	SetTargetFPS(60);
+	SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+
+	InitWindow(960, 600, "Starter Raylib");
 
 	rlImGuiSetup(true);
 
-	while (!WindowShouldClose())
-	{
-		BeginDrawing();
-		{
-			ClearBackground(RAYWHITE);
-			DrawText("Hello World!", 20, 30, 24, BLACK);
-			DrawFPS(0, 0);
+#if defined(PLATFORM_WEB)
+    emscripten_set_main_loop(mainLoop, 0, 1);
+#else
+    SetTargetFPS(60);
 
-			rlImGuiBegin();
-			{
-				ImGui::ShowDemoWindow();
-			}
-			rlImGuiEnd();
-		}
-		EndDrawing();
-	}
+    while (!WindowShouldClose())
+    {
+        mainLoop();
+    }
+#endif
 
 	rlImGuiShutdown();
 
+	CloseWindow();
 	return 0;
+}
+
+void mainLoop(void)
+{
+	BeginDrawing();
+	{
+		ClearBackground(RAYWHITE);
+		DrawText("Hello World!", 20, 30, 24, BLACK);
+		DrawFPS(0, 0);
+
+		rlImGuiBegin();
+		{
+			ImGui::ShowDemoWindow();
+		}
+		rlImGuiEnd();
+	}
+	EndDrawing();
 }
