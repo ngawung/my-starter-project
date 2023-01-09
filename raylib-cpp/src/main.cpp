df51f40e76dc19f9dfa577cf9d26376bf1c8d5fc
Ferdian <ferdian.artwork@gmail.com>
2023-01-09 23:39:24 +0700

    add android build

--- raylib-cpp/src/main.cpp
+++ raylib-cpp/src/main.cpp
@@ -1,24 +1,52 @@
+#define RLGL_IMPLEMENTATION
+#define IMGUI_IMPL_OPENGL_ES2
+
 #include <iostream>
 #include <imgui.h>
 #include <raylib.h>
 #include <rlImGui.h>
 
-#if defined(PLATFORM_WEB)
+#include "android_native_app_glue.h"
+#include "backends/imgui_impl_android.h"
+#include "imgui_impl_opengl3.h"
+
+#ifdef PLATFORM_WEB
     #include <emscripten/emscripten.h>
 #endif
 
 void mainLoop(void);
 
+extern "C" struct android_app *GetAndroidApp(void);
+
+static int32_t handleInputEvent(struct android_app* app, AInputEvent* inputEvent)
+{
+    return ImGui_ImplAndroid_HandleInputEvent(inputEvent);
+}
+
 int main()
 {
-
 	SetConfigFlags(FLAG_WINDOW_RESIZABLE);
 
-	InitWindow(960, 600, "Starter Raylib");
+	InitWindow(0, 0, "Starter Raylib");
 
-	rlImGuiSetup(true);
+	// rlImGuiSetup(true);
+    GetAndroidApp()->onInputEvent = handleInputEvent;
 
-#if defined(PLATFORM_WEB)
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO();
+	io.IniFilename = NULL;
+
+	ImGui::StyleColorsDark();
+	ImGui_ImplAndroid_Init(GetAndroidApp()->window);
+	ImGui_ImplOpenGL3_Init("#version 300 es");
+
+	ImFontConfig font_cfg;
+	font_cfg.SizePixels = 22.0f;
+	io.Fonts->AddFontDefault(&font_cfg);
+	ImGui::GetStyle().ScaleAllSizes(3.0f);
+
+#ifdef PLATFORM_WEB
     emscripten_set_main_loop(mainLoop, 0, 1);
 #else
     SetTargetFPS(60);
@@ -29,7 +57,10 @@ int main()
     }
 #endif
 
-	rlImGuiShutdown();
+	// rlImGuiShutdown();
+	ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplAndroid_Shutdown();
+    ImGui::DestroyContext();
 
 	CloseWindow();
 	return 0;
@@ -37,17 +68,27 @@ int main()
 
 void mainLoop(void)
 {
+	ImGuiIO& io = ImGui::GetIO();
+
 	BeginDrawing();
 	{
 		ClearBackground(RAYWHITE);
 		DrawText("Hello World!", 20, 30, 24, BLACK);
+		DrawText(TextFormat("%i %i, %i %i", GetScreenWidth(), GetScreenHeight(), ANativeWindow_getWidth(GetAndroidApp()->window), ANativeWindow_getHeight(GetAndroidApp()->window)), 20, 50, 24,BLACK);
+		// ImVec2 p = ImGui::GetCursorPos();
+		// DrawText(TextFormat("%i %i", (int)p.x, (int)p.y), 20, 80, 24,BLACK);
 		DrawFPS(0, 0);
 
-		rlImGuiBegin();
+		// rlImGuiBegin();
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplAndroid_NewFrame();
+		ImGui::NewFrame();
 		{
 			ImGui::ShowDemoWindow();
 		}
-		rlImGuiEnd();
+		ImGui::Render();
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+		// rlImGuiEnd();
 	}
 	EndDrawing();
 }
