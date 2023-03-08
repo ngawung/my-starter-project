81b3d1a5a7f996ee5a6187b24284827283d2af00
Ngawung <ferdian.artwork@gmail.com>
2023-03-08 09:59:30 +0700

    add arm7

--- nds-starter/src/main.cpp
+++ nds-starter/src/main.cpp
@@ -1,19 +1,35 @@
-#include <iostream>
+/*---------------------------------------------------------------------------------
+
+	Simple console print demo
+	-- dovoto
+
+---------------------------------------------------------------------------------*/
 #include <nds.h>
+#include <nds/arm9/console.h>
+#include <nds/arm9/input.h>
+#include <stdio.h>
 
+//---------------------------------------------------------------------------------
+int main(void) {
+//---------------------------------------------------------------------------------
+	touchPosition touch;
 
-int main()
-{
-	nocashMessage("Hello world!");
+	consoleDemoInit();  //setup the sub screen for printing
 
-	consoleDemoInit();
-	std::cout << "Hello World!";
+	iprintf("\n\n\tHello DS dev'rs\n");
+	iprintf("\twww.drunkencoders.com\n");
+	iprintf("\twww.devkitpro.org");
 
-	while(true)
-	{
+	while(1) {
+
+		touchRead(&touch);
+		iprintf("\x1b[10;0HTouch x = %04i, %04i\n", touch.rawx, touch.px);
+		iprintf("Touch y = %04i, %04i\n", touch.rawy, touch.py);
+
 		swiWaitForVBlank();
+		scanKeys();
+		// if (keysDown()&KEY_START) break;
 	}
 
 	return 0;
-
 }
